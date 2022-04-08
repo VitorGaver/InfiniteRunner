27c7b8c2f2135df7361decf85e016c179b412f43
Eduardo Fernandes <duardo.fernandessbr@gmail.com>
2022-04-07 21:53:53 -0300

    Adicionado sistema de movimentação
    sistema de player, movimentação em geral e colisão com objetos.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("ScenesSpawn")]
     public GameObject[] Scenes;
-
     public int selectedScene;
+    int lastScene;
+
+    [Header("PlayerSpawn")]
+    public GameObject Player;
+    public GameObject spawnPlayerPos;
 
     void Start()
     {
         Instantiate(Scenes[0], new Vector3(0, 0, 0), Quaternion.identity);
+        lastScene = 0;
         SpawnScene(new Vector3(0, 10, 0));
+        Instantiate(Player, spawnPlayerPos.transform.position, Quaternion.identity);
     }
     
     public void SpawnScene(Vector3 spawnPos)
     {
-        selectedScene = Random.Range(1, 3);
-        Instantiate(Scenes[selectedScene], spawnPos, Quaternion.identity);
+        while (selectedScene == lastScene)
+        {
+            selectedScene = Random.Range(0, 3);
+        }
+            Instantiate(Scenes[selectedScene], spawnPos, Quaternion.identity);
+            lastScene = selectedScene;
     }
 }
